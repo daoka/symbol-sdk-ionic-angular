9a89637a3866e41b3f5e641cc3bbf03b57c575a6
daoka <daoka.sweep@gmail.com>
2020-04-20 23:13:04 +0900

    WIP: import symbol-sdk

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Address } from 'symbol-sdk';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,9 @@ export class HomePage {
 
   constructor() {}
 
+  run() {
+    console.log('run');
+    const address =  Address.createFromRawAddress('TAV7RH-6LSQJA-Q44CJX-GJADCL-UU6EBR-SSYHNP-VX7U');
+    console.log(`address: ${address.plain()}`);
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,10 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+(window as any).global = window;
+(window as any).process = window;
+(window as any).process.browser = true;
+(window as any).process.version = '';
+(window as any).process.versions = {node: false};
+declare var global: any;
+global.Buffer = global.Buffer || require('buffer').Buffer;
